66a722c188dc0b3e9d7870b963efdf462b3088ba
AnuragHiware <anuraghivare555@gmail.com>
2022-03-25 19:00:49 +0530

    Added more htmls

---
+++ Registration_Form.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Registration Form </title>
+</head>
+<body>
+    <div class="main">
+        <div class="register">
+        <h2> Register Here </h2>
+        <form id="register" method="post">
+            <label> Username : </label>
+            <br>
+            <input type="text" name="name"
+            id="name" placeholder="Enter Your Name">
+            <br><br>
+            <label> Email: </label>
+            <br>
+            <input type="email" name="email"
+            id="Email" placeholder="Enter Your Email">
+            <br><br>
+            <label> Mobile No : </label>
+            <br>
+            <input type="number" name="mnumber"
+            id="mobnumber" placeholder="Enter Mobile Number Here">
+            <br><br>
+            <label> Enter OTP : </label>
+            <br>
+            <input type="password" name="otp"
+            id="otp" placeholder="Enter Verification Code">
+            <br><br>
+
+            <label> Log In : </label>
+            <br>
+            <input type="login" name="login"
+            id="login" >
+            <br><br>
+            <label> Sign Up : </label>
+            <br>
+            <input type="signup" name="sign"
+            id="signup" >
+            <br><br>
+
+            
+</form>
+
+</div> 
+</div>
+</body>
+</html>
+
+
